cb27336baf049b5c566d9a3e5cef0522c495a6d4
akinolaogunlana <akinolaogunlana@users.noreply.github.com>
2025-07-01 21:37:33 +0000

    deploy: d4b350c62bd0cfcdc237d02bde0946e465b938e1

--- posts/post-meta.js
+++ posts/post-meta.js
@@ -3,12 +3,12 @@ window.postMetadata = {
     "title": "Free-Lesson-Plan-Templates",
     "description": "for meta tag use only – Discover free, customizable Google Docs lesson plan templates that save teachers time and elevate classroom planning.",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-01T21:26:34.989Z"
+    "published": "2025-07-01T21:37:31.256Z"
   },
   "Google-Docs-Student-Planner-Template": {
     "title": "Google-Docs-Student-Planner-Template",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-01T21:26:34.990Z"
+    "published": "2025-07-01T21:37:31.257Z"
   }
 };
