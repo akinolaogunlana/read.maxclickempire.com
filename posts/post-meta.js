8e15806e22605c4f3d989db651074b11caeb4d65
akinolaogunlana <akinolaogunlana@users.noreply.github.com>
2025-07-03 14:11:41 +0000

    deploy: 029ff67bc82d52c0f50c1f059529ea711f3d4ea5

--- posts/post-meta.js
+++ posts/post-meta.js
@@ -3,18 +3,18 @@ window.postMetadata = {
     "title": "Classroom Seating Chart Google Docs Template: Free & Customizable",
     "description": "Free Classroom Seating Chart Google Docs Template. Customize seating, share with teachers, and organize your classroom easily with our drag-and-drop editable chart.",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-03T13:56:45.079Z"
+    "published": "2025-07-03T14:11:38.816Z"
   },
   "Free-Lesson-Plan-Templates": {
     "title": "Free-Lesson-Plan-Templates",
     "description": "for meta tag use only – Discover free, customizable Google Docs lesson plan templates that save teachers time and elevate classroom planning.",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-03T13:56:45.079Z"
+    "published": "2025-07-03T14:11:38.816Z"
   },
   "Google-Docs-Student-Planner-Template": {
     "title": "Google-Docs-Student-Planner-Template",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-03T13:56:45.079Z"
+    "published": "2025-07-03T14:11:38.816Z"
   }
 };
