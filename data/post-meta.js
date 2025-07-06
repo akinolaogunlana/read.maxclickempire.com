ea0bd04f55b16389e602bfa3c61c77df1875346d
akinolaogunlana <akinolaogunlana@users.noreply.github.com>
2025-07-06 20:51:35 +0000

    deploy: 9b71af8c86a0330378e74507ef8107ffcf357910

--- data/post-meta.js
+++ data/post-meta.js
@@ -3,36 +3,36 @@ window.postMetadata = {
     "title": "5-modern-resume-templates-in-google-docs-that-get-jobs",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T20:46:56.915Z"
-  },
-  "free-lesson-plan-templates": {
-    "title": "Free-Lesson-Plan-Templates",
-    "description": "for meta tag use only – Discover free, customizable Google Docs lesson plan templates that save teachers time and elevate classroom planning.",
-    "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T20:46:56.917Z"
+    "published": "2025-07-06T20:51:28.847Z"
   },
   "google-docs-student-planner-template": {
     "title": "Google-Docs-Student-Planner-Template",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T20:46:56.917Z"
+    "published": "2025-07-06T20:51:28.849Z"
   },
   "resume--cover-letter-templates": {
     "title": "Resume & Cover-Letter-Templates",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T20:46:56.918Z"
+    "published": "2025-07-06T20:51:28.849Z"
   },
   "best-free-google-docs-resume-templates": {
     "title": "best-free-google-docs-resume-templates",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T20:46:56.918Z"
+    "published": "2025-07-06T20:51:28.849Z"
   },
   "classroom-seating-chart-google-docs-template": {
     "title": "Classroom Seating Chart Google Docs Template: Free & Customizable",
     "description": "Free Classroom Seating Chart Google Docs Template. Customize seating, share with teachers, and organize your classroom easily with our drag-and-drop editable chart.",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T20:46:56.918Z"
+    "published": "2025-07-06T20:51:28.850Z"
+  },
+  "free-lesson-plan-templates": {
+    "title": "free-lesson-plan-templates",
+    "description": "for meta tag use only – Discover free, customizable Google Docs lesson plan templates that save teachers time and elevate classroom planning.",
+    "image": "https://read.maxclickempire.com/assets/og-image.jpg",
+    "published": "2025-07-06T20:51:28.850Z"
   }
 };
