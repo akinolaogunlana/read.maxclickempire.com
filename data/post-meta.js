599ce4ed056b6db3cd38cb577f88c13bcdcfced8
akinolaogunlana <akinolaogunlana@users.noreply.github.com>
2025-07-06 20:47:04 +0000

    deploy: a28635bd7895f92508dfc81e491af34526e33ff7

--- data/post-meta.js
+++ data/post-meta.js
@@ -3,36 +3,36 @@ window.postMetadata = {
     "title": "5-modern-resume-templates-in-google-docs-that-get-jobs",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T20:36:25.813Z"
-  },
-  "classroom-seating-chart-google-docs-template": {
-    "title": "Classroom Seating Chart Google Docs Template: Free & Customizable",
-    "description": "Free Classroom Seating Chart Google Docs Template. Customize seating, share with teachers, and organize your classroom easily with our drag-and-drop editable chart.",
-    "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T20:36:25.815Z"
+    "published": "2025-07-06T20:46:56.915Z"
   },
   "free-lesson-plan-templates": {
     "title": "Free-Lesson-Plan-Templates",
     "description": "for meta tag use only – Discover free, customizable Google Docs lesson plan templates that save teachers time and elevate classroom planning.",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T20:36:25.815Z"
+    "published": "2025-07-06T20:46:56.917Z"
   },
   "google-docs-student-planner-template": {
     "title": "Google-Docs-Student-Planner-Template",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T20:36:25.815Z"
+    "published": "2025-07-06T20:46:56.917Z"
   },
   "resume--cover-letter-templates": {
     "title": "Resume & Cover-Letter-Templates",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T20:36:25.815Z"
+    "published": "2025-07-06T20:46:56.918Z"
   },
   "best-free-google-docs-resume-templates": {
     "title": "best-free-google-docs-resume-templates",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T20:36:25.816Z"
+    "published": "2025-07-06T20:46:56.918Z"
+  },
+  "classroom-seating-chart-google-docs-template": {
+    "title": "Classroom Seating Chart Google Docs Template: Free & Customizable",
+    "description": "Free Classroom Seating Chart Google Docs Template. Customize seating, share with teachers, and organize your classroom easily with our drag-and-drop editable chart.",
+    "image": "https://read.maxclickempire.com/assets/og-image.jpg",
+    "published": "2025-07-06T20:46:56.918Z"
   }
 };
