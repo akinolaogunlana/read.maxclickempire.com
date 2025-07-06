91fdda6d6f7521f8b45479e8f39bcf96b990ea98
akinolaogunlana <akinolaogunlana@users.noreply.github.com>
2025-07-06 20:02:31 +0000

    deploy: 4779fa59ea1a11c00fd63c757576f7ba5cc6dc6a

--- data/post-meta.js
+++ data/post-meta.js
@@ -3,24 +3,30 @@ window.postMetadata = {
     "title": "Classroom Seating Chart Google Docs Template: Free & Customizable",
     "description": "Free Classroom Seating Chart Google Docs Template. Customize seating, share with teachers, and organize your classroom easily with our drag-and-drop editable chart.",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T19:44:17.065Z"
+    "published": "2025-07-06T20:02:25.017Z"
   },
   "free-lesson-plan-templates": {
     "title": "Free-Lesson-Plan-Templates",
     "description": "for meta tag use only – Discover free, customizable Google Docs lesson plan templates that save teachers time and elevate classroom planning.",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T19:44:17.067Z"
+    "published": "2025-07-06T20:02:25.019Z"
   },
   "google-docs-student-planner-template": {
     "title": "Google-Docs-Student-Planner-Template",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T19:44:17.067Z"
+    "published": "2025-07-06T20:02:25.019Z"
   },
   "resume--cover-letter-templates": {
     "title": "Resume & Cover-Letter-Templates",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T19:44:17.067Z"
+    "published": "2025-07-06T20:02:25.019Z"
+  },
+  "best-free-google-docs-resume-templates": {
+    "title": "best-free-google-docs-resume-templates",
+    "description": "",
+    "image": "https://read.maxclickempire.com/assets/og-image.jpg",
+    "published": "2025-07-06T20:02:25.019Z"
   }
 };
