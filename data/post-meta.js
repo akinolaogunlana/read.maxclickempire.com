cff082d771c7001b72ab5a3a1e3186b240b99f9a
akinolaogunlana <akinolaogunlana@users.noreply.github.com>
2025-07-06 20:36:32 +0000

    deploy: fd547abd866b6f5842c432f18d2eea78b759e135

--- data/post-meta.js
+++ data/post-meta.js
@@ -1,38 +1,38 @@
 window.postMetadata = {
-  "5-modern-resume-templates-in-oogle-docs-that-get-jobs": {
-    "title": "5-modern-resume templates-in-oogle-docs-that-get-jobs",
+  "5-modern-resume-templates-in-google-docs-that-get-jobs": {
+    "title": "5-modern-resume-templates-in-google-docs-that-get-jobs",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T20:14:16.916Z"
+    "published": "2025-07-06T20:36:25.813Z"
   },
   "classroom-seating-chart-google-docs-template": {
     "title": "Classroom Seating Chart Google Docs Template: Free & Customizable",
     "description": "Free Classroom Seating Chart Google Docs Template. Customize seating, share with teachers, and organize your classroom easily with our drag-and-drop editable chart.",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T20:14:16.918Z"
+    "published": "2025-07-06T20:36:25.815Z"
   },
   "free-lesson-plan-templates": {
     "title": "Free-Lesson-Plan-Templates",
     "description": "for meta tag use only – Discover free, customizable Google Docs lesson plan templates that save teachers time and elevate classroom planning.",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T20:14:16.918Z"
+    "published": "2025-07-06T20:36:25.815Z"
   },
   "google-docs-student-planner-template": {
     "title": "Google-Docs-Student-Planner-Template",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T20:14:16.918Z"
+    "published": "2025-07-06T20:36:25.815Z"
   },
   "resume--cover-letter-templates": {
     "title": "Resume & Cover-Letter-Templates",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T20:14:16.919Z"
+    "published": "2025-07-06T20:36:25.815Z"
   },
   "best-free-google-docs-resume-templates": {
     "title": "best-free-google-docs-resume-templates",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T20:14:16.919Z"
+    "published": "2025-07-06T20:36:25.816Z"
   }
 };
