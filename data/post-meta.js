2a7cdc487bcd071764c4c26fa1bf9505f1a0c344
akinolaogunlana <akinolaogunlana@users.noreply.github.com>
2025-07-06 16:12:25 +0000

    deploy: 94d69471c4b52f11443d1edf7d9769fd2974e21c

--- data/post-meta.js
+++ data/post-meta.js
@@ -1,32 +1,32 @@
 window.postMetadata = {
-  "Best free GoFree-Best-Google-Docs-Resume-Templates": {
+  "best-free-gofree-best-google-docs-resume-templates": {
     "title": "Best free GoFree-Best-Google-Docs-Resume-Templates",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T15:00:53.351Z"
+    "published": "2025-07-06T16:12:19.993Z"
   },
-  "Classroom-Seating-Chart-Google-Docs-Template": {
+  "classroom-seating-chart-google-docs-template": {
     "title": "Classroom Seating Chart Google Docs Template: Free & Customizable",
     "description": "Free Classroom Seating Chart Google Docs Template. Customize seating, share with teachers, and organize your classroom easily with our drag-and-drop editable chart.",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T15:00:53.353Z"
+    "published": "2025-07-06T16:12:19.995Z"
   },
-  "Free-Lesson-Plan-Templates": {
+  "free-lesson-plan-templates": {
     "title": "Free-Lesson-Plan-Templates",
     "description": "for meta tag use only – Discover free, customizable Google Docs lesson plan templates that save teachers time and elevate classroom planning.",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T15:00:53.354Z"
+    "published": "2025-07-06T16:12:19.995Z"
   },
-  "Google-Docs-Student-Planner-Template": {
+  "google-docs-student-planner-template": {
     "title": "Google-Docs-Student-Planner-Template",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T15:00:53.354Z"
+    "published": "2025-07-06T16:12:19.995Z"
   },
-  "Resume & Cover-Letter-Templates": {
+  "resume--cover-letter-templates": {
     "title": "Resume & Cover-Letter-Templates",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-06T15:00:53.354Z"
+    "published": "2025-07-06T16:12:19.996Z"
   }
 };
