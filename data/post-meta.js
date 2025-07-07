29ec1884c4150257e58b253897f573971d8c8983
akinolaogunlana <akinolaogunlana@users.noreply.github.com>
2025-07-07 10:25:26 +0000

    deploy: 8d9e2367d41565bda8523bcba674e5454a6fee7a

--- data/post-meta.js
+++ data/post-meta.js
@@ -3,36 +3,42 @@ window.postMetadata = {
     "title": "5-modern-resume-templates-in-google-docs-that-get-jobs",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-07T10:14:06.124Z"
+    "published": "2025-07-07T10:25:20.366Z"
   },
   "best-free-google-docs-resume-templates": {
     "title": "best-free-google-docs-resume-templates",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-07T10:14:06.126Z"
+    "published": "2025-07-07T10:25:20.368Z"
   },
   "classroom-seating-chart-google-docs-template": {
     "title": "Classroom Seating Chart Google Docs Template: Free & Customizable",
     "description": "Free Classroom Seating Chart Google Docs Template. Customize seating, share with teachers, and organize your classroom easily with our drag-and-drop editable chart.",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-07T10:14:06.126Z"
+    "published": "2025-07-07T10:25:20.368Z"
   },
   "free-lesson-plan-templates": {
     "title": "free-lesson-plan-templates",
     "description": "for meta tag use only – Discover free, customizable Google Docs lesson plan templates that save teachers time and elevate classroom planning.",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-07T10:14:06.127Z"
+    "published": "2025-07-07T10:25:20.369Z"
+  },
+  "google-docs-resume-template-for-fresh-graduates": {
+    "title": "google-docs-resume template-for-fresh-graduates",
+    "description": "",
+    "image": "https://read.maxclickempire.com/assets/og-image.jpg",
+    "published": "2025-07-07T10:25:20.369Z"
   },
   "hoogle-docs-student-planner-template": {
     "title": "hoogle-docs-student-planner-template",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-07T10:14:06.127Z"
+    "published": "2025-07-07T10:25:20.369Z"
   },
   "resume-and-cover-letter-templates": {
     "title": "resume-and-cover-letter-templates",
     "description": "",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-07T10:14:06.127Z"
+    "published": "2025-07-07T10:25:20.369Z"
   }
 };
