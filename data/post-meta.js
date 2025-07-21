27b7b1db311824d53bfc1336e2f63b33d73bfe65
akinolaogunlana <akinolaogunlana@users.noreply.github.com>
2025-07-21 19:14:52 +0000

    deploy: c56cafaff139ff68c47dc8ad7c56b42bb017f484

--- data/post-meta.js
+++ data/post-meta.js
@@ -5,19 +5,19 @@ const postMetadata = {
     "title": "How to Create an ATS‑Friendly Resume on Google Docs",
     "description": "A step‑by‑step guide to building a clean, ATS‑friendly resume in Google Docs that lands interviews. Learn formatting best practices, templates, examples, and download tips.",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-21T18:56:13.457Z"
+    "published": "2025-07-21T19:14:47.070Z"
   },
   "best-free-resume-templates-docs": {
     "title": "15 Best Free Google Docs Resume Templates That Land Jobs",
     "description": "A post from MaxClickEmpire.",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-21T18:56:13.459Z"
+    "published": "2025-07-21T19:14:47.072Z"
   },
   "modern-resume-templates-get-jobs": {
     "title": "Modern Resume Templates in Google Docs That Get Jobs",
     "description": "A post from MaxClickEmpire.",
     "image": "https://read.maxclickempire.com/assets/og-image.jpg",
-    "published": "2025-07-21T18:56:13.459Z"
+    "published": "2025-07-21T19:14:47.072Z"
   }
 };
 
