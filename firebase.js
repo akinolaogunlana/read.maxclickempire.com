9b024199d52782d8158fedae7ee1e5b985aa595a
akinolaogunlana <142597105+akinolaogunlana@users.noreply.github.com>
2025-08-03 21:53:11 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,14 @@
 // firebase.js
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp, cert } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import serviceAccount from "./serviceAccountKey.json" assert { type: "json" };
 
-// 🔁 Use your Firebase config here
-const firebaseConfig = {
-  apiKey: "AIzaSyCqgfH_fX69EqBe5iOmN7F1E09gGj4zW60",
-  authDomain: "animated-way-426007-p6.firebaseapp.com",
-  databaseURL: "https://animated-way-426007-p6-default-rtdb.firebaseio.com",
-  projectId: "animated-way-426007-p6",
-  storageBucket: "animated-way-426007-p6.firebasestorage.app",
-  messagingSenderId: "33587602209",
-  appId: "1:33587602209:web:818e58f30a2886eb4b9460",
-  measurementId: "G-LGG4K6E22F"
-};
+// Initialize Firebase Admin SDK
+initializeApp({
+  credential: cert(serviceAccount),
+});
 
-// ✅ Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+// Get Firestore instance
+const db = getFirestore();
 
 export { db };
