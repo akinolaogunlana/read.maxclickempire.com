abe4617547266cdf285b2b1e1c24ce42621d4477
akinolaogunlana <142597105+akinolaogunlana@users.noreply.github.com>
2025-08-03 21:13:05 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,21 @@
 // firebase.js
-const admin = require("firebase-admin");
-const serviceAccount = require("./credentials.json");
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+// 🔁 Use your Firebase config here
+const firebaseConfig = {
+  apiKey: "AIzaSyCqgfH_fX69EqBe5iOmN7F1E09gGj4zW60",
+  authDomain: "animated-way-426007-p6.firebaseapp.com",
+  databaseURL: "https://animated-way-426007-p6-default-rtdb.firebaseio.com",
+  projectId: "animated-way-426007-p6",
+  storageBucket: "animated-way-426007-p6.firebasestorage.app",
+  messagingSenderId: "33587602209",
+  appId: "1:33587602209:web:818e58f30a2886eb4b9460",
+  measurementId: "G-LGG4K6E22F"
+};
 
-const db = admin.firestore();
-module.exports = db;
+// ✅ Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
